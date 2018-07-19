e94ba6c1a464796ce09d89dff5b9830b5171dd9b
abody amer <abdullha.eshaq94@gmail.com>
2018-07-20 02:58:37 +0800

    sdsdf

--- view/src/components/app.js
+++ view/src/components/app.js
@@ -5,7 +5,7 @@ class App extends Component{
     constructor(props){
         super(props)
         this.state = {
-            message: 'Welcome'
+            message: 'Rebuild'
         }
         this.click = this.click.bind(this)
     }
