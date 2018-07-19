8cb5b503eca8eeb8e8a6c62be56193ef633caade
abody amer <abdullha.eshaq94@gmail.com>
2018-07-20 02:47:54 +0800

    fdsafdsaD

--- view/src/index.js
+++ view/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import AllRoutes from './components/another'
+import AllRoutes from './components/routes'
 
 ReactDOM.render(
 <AllRoutes /> , document.querySelector('#app')
