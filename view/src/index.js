435597f8bd17c70928f2330cdab6cca7b4593df2
abody amer <abdullha.eshaq94@gmail.com>
2018-07-20 03:22:27 +0800

    A

--- view/src/index.js
+++ view/src/index.js
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom'
 import AllRoutes from './components/routes'
 
 ReactDOM.render(
-<h1>What is wrong</h1> , document.querySelector('#app')
+<AllRoutes /> , document.querySelector('#app')
 )
