08081547c2e574628b469bf0f900a681ada881d6
abody amer <abdullha.eshaq94@gmail.com>
2018-07-19 23:47:24 +0800

    c

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
 // });
 var express = require('express')
 var app = express()
-app.use(express.static('build'))
+app.use(express.static( __dirname, 'view/build'))
 app.get('/api/getMessage' , (req, res) => {
    
     res.json({"name": "abody"})
